3998d62aecc0bebc8b37caee9c3b1c4ca3e91370
greemaltend4 <dengestabiy@hotmail.com>
2025-08-28 21:20:06 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gestion de Factures Cloud</h1>
+      <p>Bienvenue dans l'application de gestion de factures !</p>
+    </div>
+  );
+}
+
+export default App;
